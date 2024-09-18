5274941dd1a96e9746150756fce8586e45cbbef8
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-18 15:40:45 +0200

    Refactor app to use components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Login from "./Login";
+
+let isLoggedIn = false;
+
+function renderConditionally() {
+    if (isLoggedIn) {
+        return <h1>Hello</h1>
+    } else {
+        return (
+            <Login />
+        );
+    }
+}
+
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            {renderConditionally()}
+        </div>
+    );
 }
 
 export default App;
