d4e53e39e5c32e9f03eb162fc103eac762df0b43
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-19 17:31:07 +0200

    Add events handling for button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import Input from './Input';
 
 function Form(props) {
+
+    const [isMousedOver, setIsMouseOver] = React.useState(false);
+
+    function handleClick() {
+        setMouseClick(true);
+    }
+
+    function handleMouseOver() {
+        setIsMouseOver(true);
+    }
+
+    function handleMouseOut() {
+        setIsMouseOver(false);
+    }
+
     return (
+        <>
+        <h1>"Welcome"</h1>
         <form className="form">
             <Input
                 type="text"
@@ -16,10 +33,12 @@ function Form(props) {
                 type="password"
                 placeholder="confirm password"
             /> }
-            <button type="submit">
+            <button type="submit" style={{backgroundColor: isMousedOver ? "black" : "white"}}
+                    onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
                 {props.isRegistered ? "Login" : "Register"}
             </button>
         </form>
+        </>
     )
 }
 
