577b4d642d092d6b28adcef86fa9bedb7bbb89b5
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-20 15:05:38 +0200

    Add email field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,8 @@ function Form(props) {
     const [isMousedOver, setIsMouseOver] = React.useState(false);
     const [userData, setData] = React.useState({
         username: "",
-        password: ""
-
+        password: "",
+        email: ""
     });
 
     function captureData(event) {
@@ -18,12 +18,20 @@ function Form(props) {
             if (name === "username") {
                 return {
                     username: value,
-                    password: prevValue.password
+                    password: prevValue.password,
+                    email: prevValue.email
                 }
             } else if (name === "password") {
                 return {
                     username: prevValue.username,
-                    password: value
+                    password: value,
+                    email: prevValue.email
+                }
+            } else if (name === "email") {
+                return {
+                    username: prevValue.username,
+                    password: prevValue.password,
+                    email: value
                 }
             }
         })
@@ -43,33 +51,41 @@ function Form(props) {
         <h1>Welcome
             <br/> username: {userData.username}
             <br/> password: {userData.password}
+            <br/> email: {userData.email}
         </h1>
-        <form className="form">
-            <input
-                name="username"
-                onChange={captureData}
-                type="text"
-                placeholder="username"
-                value={userData.username}
-            />
-            <input
-                name="password"
-                onChange={captureData}
-                type="password"
-                placeholder="password"
-                value={userData.password}
-            />
-            {!props.isRegistered && <Input
-                type="password"
-                placeholder="confirm password"
-            /> }
-            <button type="submit" style={{backgroundColor: isMousedOver ? "black" : "white"}}
-                    onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-               {props.isRegistered ? "Login" : "Register"}
-            </button>
-        </form>
+            <form className="form">
+                <input
+                    name="username"
+                    onChange={captureData}
+                    type="text"
+                    placeholder="username"
+                    value={userData.username}
+                />
+                <input
+                    name="password"
+                    onChange={captureData}
+                    type="password"
+                    placeholder="password"
+                    value={userData.password}
+                />
+                <input
+                    name="email"
+                    onChange={captureData}
+                    type="email"
+                    placeholder="email"
+                    value={userData.email}
+                />
+                {!props.isRegistered && <Input
+                    type="password"
+                    placeholder="confirm password"
+                />}
+                <button type="submit" style={{backgroundColor: isMousedOver ? "black" : "white"}}
+                        onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+                    {props.isRegistered ? "Login" : "Register"}
+                </button>
+            </form>
         </>
     )
-}
+    }
 
-export default Form;
+    export default Form;
